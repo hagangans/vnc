a1f4981c015fdb056cb0c6b127246d3915dcbbcf
hagangans <104121511+hagangans@users.noreply.github.com>
2022-12-31 03:30:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6281210258584'] //ur owner number
+global.ownername = "🦄HAGAN X BOT" //ur owner name
+global.ytname = "HAGAN GANS" //ur yt chanel name
+global.socialm = "IG: MR.HAGAN_404CR4ZY" //ur github or insta name
+global.location = "India, Mizoram, Aizawl, Indonesia" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
+global.ownernomer = "6281210258584" //ur number
+global.premium = ['6281210258584'] //ur premium number
+global.botname = 'HAGAN X BOT' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "HAGAN X BOT\n\n\nA whatsapp bot developed by\n🦄HAGAN GANS\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +6281210258584" //ur sticker watermark author
+global.wm = "HAGAN X BOT Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
